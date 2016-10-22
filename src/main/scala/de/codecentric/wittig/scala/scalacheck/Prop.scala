d392e8f3fc9070281e075b3da700a5e462269e40
Gunther Wittig <gunther.wittig@codecentric.de>
2016-10-22 14:54:52 +0200

    added travis file

---
+++ src/main/scala/de/codecentric/wittig/scala/scalacheck/Prop.scala
@@ -0,0 +1,12 @@
+package de.codecentric.wittig.scala.scalacheck
+
+class Prop {
+  def check(): Unit = ???
+}
+object Prop {
+  def forAll[A](prop: A => Boolean) = ???
+}
+
+object Main {
+  Prop.forAll { (x: Int) => x > 2 }
+}
