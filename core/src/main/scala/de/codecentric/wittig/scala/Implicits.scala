7e85aba6dbd85156563399fff0ed214222d9b56d
Gunther Wittig <gunther.wittig@codecentric.de>
2020-07-06 08:30:53 +0200

    Test with different Schedulers

--- core/src/main/scala/de/codecentric/wittig/scala/Implicits.scala
+++ core/src/main/scala/de/codecentric/wittig/scala/Implicits.scala
@@ -4,14 +4,14 @@ import scala.concurrent.{Await, Future}
 
 object Implicits {
   implicit class RichFuture[T](future: Future[T]) {
-    def await(implicit duration: Duration = 10 seconds): T = Await.result(future, duration)
+    def await(implicit duration: Duration = 10.seconds): T = Await.result(future, duration)
   }
 
-  def time[A](a: => A) = {
+  def time[A](a: => A): A = {
     val now    = System.nanoTime
     val result = a
     val micros = (System.nanoTime - now) / 1000000L
-    println("%d Mikrosekunden".format(micros))
+    println("%d Millisekunden".format(micros))
     result
   }
 }
--- core/src/main/scala/de/codecentric/wittig/scala/monix/MonixWithScheduler.scala
+++ core/src/main/scala/de/codecentric/wittig/scala/monix/MonixWithScheduler.scala
@@ -5,34 +5,41 @@ import cats.implicits._
 import de.codecentric.wittig.scala.Implicits._
 import de.codecentric.wittig.scala.Printer._
 import monix.execution.Scheduler
+
+import scala.annotation.tailrec
+
 object MonixWithScheduler extends TaskApp {
 
-  private val list        = List.tabulate(10)(i => 1000000 + i)
-  private val schedulerIO = Scheduler.io(name = "schedulerIO")
-  private val scheduler02 = Scheduler.computation(name = "scheduler02", parallelism = 2)
+  private val list                    = List.tabulate(10)(i => 1000000 + i)
+  private val schedulerIO             = Scheduler.io(name = "io")
+  private val schedulerSingleThreaded = Scheduler.singleThread(name = "singleThread")
+  private val schedulerComputation02  = Scheduler.computation(name = "computation2", parallelism = 2)
+  private val schedulerComputation04  = Scheduler.computation(name = "computation4", parallelism = 4)
+
+  private val task1 = parallel(printlnBlue).executeOn(schedulerComputation04)
+  private val task2 = parallel(printlnCyan)
+
+  private def programm = (task1, task2).parMapN {
+    case (list1, list2) => list1.sum + list2.sum
+  }
 
   override def run(args: List[String]): Task[ExitCode] =
     programm.as(ExitCode.Success)
 
-  def programm = (task1, task2).parMapN { case (list1, list2) => () }
-  val task1    = parallel(printlnBlue).executeOn(scheduler02)
-  val task2    = parallel(printlnCyan)
-
-  def parallel(printlnColored: String => Unit): Task[List[BigInt]] =
-    list.zipWithIndex.parTraverse {
-      case (i, index) =>
-        Task(fibonacci(i)).map { p =>
-          printlnColored(s"Lauf: $index,  ThreadId: ${Thread.currentThread().getId}")
-          p
-        }
-    }
+  private def parallel(printlnColored: String => Unit): Task[List[BigInt]] =
+    list.zipWithIndex
+      .parTraverse {
+        case (i, index) =>
+          Task(fibonacci(i)) <*
+            Task(printlnColored(s"Lauf: $index,  ThreadId: ${Thread.currentThread().getId}"))
+      }
 
   /** Wird nur als großer CPU-Verbraucher benötigt. Inhalt unwichtig; verbraucht CPU */
   private def fibonacci(i: BigInt): BigInt = time {
-    def h(last: BigInt, cur: BigInt, num: BigInt): BigInt = {
+    @tailrec
+    def h(last: BigInt, cur: BigInt, num: BigInt): BigInt =
       if (num == 0) cur
       else h(cur, last + cur, num - 1)
-    }
 
     if (i < 0) -1
     else if (i == 0 || i == 1) 1
