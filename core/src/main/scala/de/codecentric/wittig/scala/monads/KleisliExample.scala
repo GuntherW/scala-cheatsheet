b6ff1b1af51108fa54b1c704ccfc0a146112a4c8
Gunther Wittig <gunther.wittig@codecentric.de>
2020-09-19 15:05:46 +0200

    Example for a Kleisli

---
+++ core/src/main/scala/de/codecentric/wittig/scala/monads/KleisliExample.scala
@@ -0,0 +1,23 @@
+package de.codecentric.wittig.scala.monads
+
+import cats.data.Kleisli
+import cats.implicits._
+
+/**
+  * Kleisli enables composition of functions that return a monadic value
+  *
+  * @see https://typelevel.org/cats/datatypes/kleisli.html
+  */
+object KleisliExample extends App {
+
+  val parse: Kleisli[Option, String, Int] =
+    Kleisli(s => if (s.matches("-?[0-9]+")) s.toInt.some else None)
+
+  val reciprocal: Kleisli[Option, Int, Double] =
+    Kleisli(i => if (i != 0) Some(1.0 / i) else None)
+
+  val parseAndReciprocal: Kleisli[Option, String, Double] =
+    reciprocal.compose(parse)
+
+  println(parseAndReciprocal.run("3"))
+}
