85024ddc8d32b9aef52ec165e91bdd61d6fe5471
Gunther Wittig <gunther.wittig@codecentric.de>
2021-03-12 08:31:37 +0100

    added examples for munit

--- munit/src/test/scala/munit/FirstTest.scala
+++ munit/src/test/scala/munit/FirstTest.scala
@@ -1,5 +1,6 @@
 package munit
 
+import java.nio.file.{Files, Path}
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.Properties
@@ -28,5 +29,43 @@ class FirstTest extends munit.FunSuite {
   test("Futures should be ") {
     val f1 = Future(1)
     f1.map(f => assertEquals(f, 1))
+  }
+
+  test("async") {
+    Future {
+      println("Hello Internet!")
+    }
+  }
+
+  test("failing test".fail) {
+    assertEquals(1, 2)
+  }
+
+  test("exception") {
+    intercept[java.lang.IllegalArgumentException] {
+      throw new IllegalArgumentException
+    }
+  }
+
+  test("exception mit message") {
+    interceptMessage[java.lang.IllegalArgumentException]("Mit Message") {
+      throw new IllegalArgumentException("Mit Message")
+    }
+  }
+
+  // Deklarieren einer Fixture
+  private val files = FunFixture[Path](
+    setup = { test =>
+      Files.createTempFile("tmp", test.name)
+    },
+    teardown = { file =>
+      // Always gets called, even if test failed.
+      Files.deleteIfExists(file)
+    }
+  )
+
+  // Benutzung einer Fixture
+  files.test("basic") { file =>
+    assert(Files.isRegularFile(file), s"Files.isRegularFile($file)")
   }
 }
