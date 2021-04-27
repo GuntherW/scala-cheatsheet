b5b04ee5efce10796617e0feec179ba8762eb63b
Gunther Wittig <gunther.wittig@codecentric.de>
2021-04-27 14:46:26 +0200

    update

---
+++ munit/src/test/scala/de/wittig/WordSpecScalaTest.scala
@@ -0,0 +1,17 @@
+package de.wittig
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class WordSpecScalaTest extends AnyWordSpecLike with Matchers {
+  case class Person(name: String, color: String)
+
+  "Person" when {
+    "retrieving name" should {
+      "return name" in {
+        val p = Person("Hans", "red")
+        p.name shouldBe "Hans"
+      }
+    }
+  }
+}
