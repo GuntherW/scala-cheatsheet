b82713517c1c057a8478ebcd82a0580e00512949
Gunther Wittig <gunther.wittig@codecentric.de>
2022-03-04 08:06:42 +0100

    Validation with cats

---
+++ core/src/main/scala/de/codecentric/wittig/scala/cat/ValidationWithNonEmptyChain.scala
@@ -0,0 +1,38 @@
+package de.codecentric.wittig.scala.cat
+
+import cats.*
+import cats.data.ValidatedNec
+import cats.implicits.*
+import cats.implicits.catsSyntaxValidatedIdBinCompat0
+
+case class User(name: String, password: String)
+object ValidationWithNonEmptyChain extends App:
+
+  type ValidationResult[A] = ValidatedNec[String, A]
+
+  private def validateUserName(userName: String): ValidationResult[String] =
+    if userName.matches("^[a-zA-Z0-9]+$") then userName.validNec
+    else "wrong username".invalidNec
+
+  private def isLowerCase(s: String): ValidationResult[String] =
+    if s == s.toLowerCase then s.validNec
+    else "not lowercase".invalidNec
+
+  private def lowerUserName(userName: String) = validateUserName(userName).andThen(isLowerCase)
+
+  private def validatePassword(password: String): ValidationResult[String] =
+    if password.matches("(?=^.{10,}$)((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$") then password.validNec
+    else "wrong password".invalidNec
+
+  val validUser = (
+    lowerUserName("username"),
+    validatePassword("passW0rd123")
+  ).mapN(User)
+
+  val invalidUser = (
+    validateUserName("%%%"),
+    validatePassword("pass")
+  ).mapN(User)
+
+  println(validUser)
+  println(invalidUser)
