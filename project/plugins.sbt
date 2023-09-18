73b13d942807de66db7b4bbebde22bd4542b5b4b
Gunther Wittig <gunther.wittig@codecentric.de>
2023-09-18 06:49:17 +0200

    WIP

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,7 +3,7 @@ addSbtPlugin("org.scalameta"    % "sbt-scalafmt"        % "2.5.2")
 addSbtPlugin("org.scalameta"    % "sbt-mdoc"            % "2.3.7")
 addSbtPlugin("com.typesafe.sbt" % "sbt-git"             % "1.0.2")
 addSbtPlugin("org.scala-js"     % "sbt-scalajs"         % "1.13.2")
-addSbtPlugin("ch.epfl.scala"    % "sbt-scalafix"        % "0.11.0")
+addSbtPlugin("ch.epfl.scala"    % "sbt-scalafix"        % "0.11.1")
 addSbtPlugin("io.gatling"       % "gatling-sbt"         % "4.5.0")
 
 libraryDependencies += "org.scala-js" %% "scalajs-env-jsdom-nodejs" % "1.1.0"
---
+++ storch/src/main/scala/de/wittig/scala/storch/Main.scala
@@ -0,0 +1,26 @@
+package de.wittig.scala.storch
+import torch.Device.{CPU, CUDA}
+import torch.*
+
+// https://storch.dev/tutorial/buildmodel.html
+object Main extends App:
+
+  val device = if torch.cuda.isAvailable then CUDA else CPU
+
+  class NeuralNetwork extends nn.Module:
+    val flatten         = nn.Flatten()
+    val linearReluStack = register(nn.Sequential(
+      nn.Linear(28 * 28, 512),
+      nn.ReLU(),
+      nn.Linear(512, 512),
+      nn.ReLU(),
+      nn.Linear(512, 10),
+    ))
+
+    def apply(x: Tensor[Float32]) =
+      val flattened = flatten(x)
+      val logits    = linearReluStack(flattened)
+      logits
+
+  val model = NeuralNetwork().to(device)
+  println(model)
