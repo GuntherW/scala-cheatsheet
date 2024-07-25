786847f86336e6d3dd7986a0f5df3baef35baaf2
Gunther Wittig <gunther.wittig@codecentric.de>
2024-07-25 18:12:42 +0200

    WIP

--- macros/src/main/scala/de/wittig/macros/annotation/Main.scala
+++
@@ -1,8 +0,0 @@
-package de.wittig.macros.annotation
-
-import scala.annotation.experimental
-
-// Will print the Tree to the console, when "sbt compile" is called.
-@experimental
-@printTree
-case class Person(name: String, alter: Int)
---
+++ quill/readme.md
@@ -0,0 +1,7 @@
+```shell
+pgcli -h localhost -p 5432 -u postgres -d persondb
+```
+
+```sql
+show TRANSACTION ISOLATION LEVEL
+```
