c4570630239f7b6eadbd05e8c4c9e14b24b8c43d
Gunther Wittig <gunther.wittig@codecentric.de>
2024-12-23 11:21:05 +0100

    WIP

---
+++ core/src/main/scala/de/codecentric/wittig/scala/extension/ImplicitConversion.scala
@@ -0,0 +1,12 @@
+package de.codecentric.wittig.scala.extension
+
+import de.codecentric.wittig.scala.extension.Main.Money
+import scala.language.implicitConversions
+
+object ImplicitConversion extends App:
+
+  given Conversion[Double, Money] = Money(_, "EUR")
+
+  private def sameCurrency(a: Money, b: Money) = a.currency == b.currency
+
+  println(sameCurrency(123.0, 499.99))
--- core/src/main/scala/de/codecentric/wittig/scala/extension/Main.scala
+++ core/src/main/scala/de/codecentric/wittig/scala/extension/Main.scala
@@ -1,18 +1,14 @@
 package de.codecentric.wittig.scala.extension
-import scala.language.implicitConversions
 
 object Main extends App:
 
-  case class Money(value: Double, currenty: String)
+  case class Money(value: Double, currency: String)
 
   extension (m: Money)
-    def +(other: Money) = Money(m.value + other.value, m.currenty)
+    def +(other: Money) = Money(m.value + other.value, m.currency)
 
   given Conversion[Double, Money] = Money(_, "EUR")
 
   val m1 = Money(123.12, "EUR")
-  val m2 = Money(123.12, "EUR")
+  val m2 = Money(100, "EUR")
   println(m1 + m2)
-
-  def sameCurrency(a: Money, b: Money) = a.currenty == b.currenty
-  println(sameCurrency(123.23, 123.32))
