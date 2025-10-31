0e90199e06a602c9bfae389dba047a7c62c26f4f
Gunther Wittig <gunther.wittig@codecentric.de>
2025-10-31 13:24:49 +0100

    WIP

--- spring/src/test/scala/JsonMappingTest.scala
+++ spring/src/test/scala/JsonMappingTest.scala
@@ -1,4 +1,4 @@
-import JsonMappingTest.{Model1, Model2}
+import JsonMappingTest.{Model2, Model3}
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
@@ -6,38 +6,45 @@ import munit.FunSuite
 
 class JsonMappingTest extends FunSuite:
 
-  test("model1 1"):
-    val mapper = ObjectMapper().registerModules(DefaultScalaModule, new JavaTimeModule)
-
-    val model = Model1(1, "name")
-    val json  = mapper.writeValueAsString(model)
-    assertEquals(json, """{"id":1,"name":"name"}""")
-
-  test("model1 2"):
-    val mapper = ObjectMapper().registerModules(DefaultScalaModule, new JavaTimeModule)
-
-    val json  = """{"id":1}"""
-    val model = Model1(1, null)
-    val m     = mapper.readValue(json, classOf[Model1])
-    assertEquals(m, model)
+  private val mapper = ObjectMapper()
+    .registerModules(DefaultScalaModule, new JavaTimeModule)
 
   test("model2 1"):
-    val mapper = ObjectMapper().registerModules(DefaultScalaModule, new JavaTimeModule)
-
-    val model = Model2(1, List("name"))
+    val model = Model2(1, List("eins"))
     val json  = mapper.writeValueAsString(model)
-    assertEquals(json, """{"id":1,"li":["name"]}""")
+    assertEquals(json, """{"id":1,"li":["eins"]}""")
 
-  test("model2 2"):
-    val mapper = ObjectMapper().registerModules(DefaultScalaModule, new JavaTimeModule)
-//      .configure(DeserializationFeature.ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT, true)
-//      .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
-
+  // Genau das ist das Standardverhalten der alten Jacksonversion (2.18.x). Das geht jetzt nicht mehr.
+  test("model2 2".ignore):
     val json  = """{"id":1}"""
-    val model = Model2(1, Nil)
+    val model = Model2(1, null)
     val m     = mapper.readValue(json, classOf[Model2])
     assertEquals(m, model)
 
+  test("model2 3"):
+    val json  = """{"id":1, "li":["eins"]}"""
+    val model = Model2(1, List("eins"))
+    val m     = mapper.readValue(json, classOf[Model2])
+    assertEquals(m, model)
+
+  test("model3 1"):
+    val json  = """{"id":1}"""
+    val model = Model3(1, null)
+    val m     = mapper.readValue(json, classOf[Model3])
+    assertEquals(m, model)
+
+  test("model3 2"):
+    val json  = """{"id":1, "li":["eins"]}"""
+    val model = Model3(1, new java.util.ArrayList(java.util.Arrays.asList("eins")))
+    val m     = mapper.readValue(json, classOf[Model3])
+    assertEquals(m, model)
+
+  test("model3 3"):
+    val json  = """{"id":1, "li":[]}"""
+    val model = Model3(1, new java.util.ArrayList())
+    val m     = mapper.readValue(json, classOf[Model3])
+    assertEquals(m, model)
+
 object JsonMappingTest:
-  case class Model1(id: Long, name: String)
   case class Model2(id: Long, li: List[String])
+  case class Model3(id: Long, li: java.util.List[String])
