7df53b6340b0038c4f3e4a53d5b6341e3f8160b2
Gunther Wittig <gunther.wittig@codecentric.de>
2026-07-15 11:12:50 +0200

    WIP

--- cli/azureCli/BlobService.scala
+++ cli/azureCli/BlobService.scala
@@ -1,10 +1,3 @@
-//> using dep com.azure:azure-sdk-bom:1.3.3
-//> using dep com.azure:azure-identity:1.18.1
-//> using dep com.azure:azure-storage-blob:12.32.0
-//> using dep xyz.matthieucourt::layoutz::0.8.0
-//> using dep com.lihaoyi::os-lib:0.11.9-M7
-//> using file Model.scala
-
 import com.azure.identity.ClientCertificateCredentialBuilder
 import com.azure.storage.blob.*
 import com.azure.storage.blob.models.ListBlobsOptions
