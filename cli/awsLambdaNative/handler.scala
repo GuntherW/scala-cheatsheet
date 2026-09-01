bdaf8161b022c226023aca08eed690f4fc5fc85d
Gunther Wittig <gunther.wittig@codecentric.de>
2026-09-01 11:24:10 +0200

    WIP

--- cli/awsLambdaNative/handler.scala
+++ cli/awsLambdaNative/handler.scala
@@ -1,4 +1,4 @@
-//> using scala "3.3.8"
+//> using scala "3.9.0"
 //> using platform "native"
 //> using nativeVersion "0.5.12"
 //> using nativeMode "release-fast"
--- cli/awsLambdaScalaJS/handler.scala
+++ cli/awsLambdaScalaJS/handler.scala
@@ -1,4 +1,4 @@
-//> using scala "3.8.4"
+//> using scala "3.9.0"
 //> using platform "js"
 //> using jsModuleKind "commonjs"
 //> using dep "org.scala-js::scalajs-dom::2.8.1"
