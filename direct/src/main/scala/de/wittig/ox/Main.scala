b23bd162e2f865a315e42311e3af789a62ec1183
Gunther Wittig <gunther.wittig@codecentric.de>
2026-09-15 07:08:14 +0200

    WIP

--- direct/src/main/scala/de/wittig/ox/Main.scala
+++ direct/src/main/scala/de/wittig/ox/Main.scala
@@ -1,44 +1,70 @@
 package de.wittig.ox
 
 import ox.*
-import ox.either.ok
+import ox.either.{catching, ok}
 import ox.resilience.*
-import ox.scheduling.Schedule
+import ox.scheduling.{repeat, Schedule}
 
+import scala.concurrent.TimeoutException
 import scala.concurrent.duration.*
 import scala.util.Random
 
 @main
 def main(): Unit =
 
+  parallel()
+  retrying()
+  eitherOx()
+  racing()
+  timeOutOx()
+  repeating()
+
   // run two computations in parallel
-  def computation1: Int =
-    sleep(2.seconds)
-    println("computation1")
-    1
+  def parallel(): Unit =
+    def computation1: Int =
+      sleep(2.seconds)
+      println("computation1")
+      1
 
-  def computation2: String =
-    sleep(1.second)
-    println("computation2")
-    "2"
+    def computation2: String =
+      sleep(1.second)
+      println("computation2")
+      "2"
 
-  val resultPar: (Int, String) = par(computation1, computation2)
-  println(resultPar)
+    val resultPar: (Int, String) = par(computation1, computation2)
+    println(resultPar)
 
-  // retry a computation
-  def computationR: Int =
-    if (Random.nextBoolean()) {
-      throw new RuntimeException("boom!")
-    } else {
-      Random.nextInt()
-    }
+  def retrying(): Unit =
+    def computationR: Int = if Random.nextBoolean() then throw new RuntimeException("boom!") else Random.nextInt()
 
-  val resultRetry = retry(Schedule.exponentialBackoff(100.millis))(computationR)
-  println(resultRetry)
+    val resultRetry = retry(Schedule.exponentialBackoff(100.millis))(computationR)
+    println(s"retrying: $resultRetry")
 
-  val v1: Either[Int, String]    = Left(1)
-  val v2: Either[String, String] = Left("eins")
+  def eitherOx(): Unit =
+    val v1: Either[Int, String]    = Left(1)
+    val v2: Either[String, String] = Left("eins")
 
-  val result: Either[Int | String, String] = either:
-    v1.ok() ++ v2.ok()
-  println(result)
+    val result: Either[Int | String, String] = either:
+      v1.ok() ++ v2.ok()
+    println(result)
+
+  def racing(): Unit =
+    def computation1: Int = { sleep(1.seconds); 1 }
+    def computation2: Int = { sleep(2.second); 2 }
+
+    val res: Int = raceSuccess(computation1, computation2)
+    println(s"racing: $res")
+
+  def timeOutOx(): Unit =
+    def computation3: Int = { sleep(2.seconds); 1 }
+
+    val res: Either[TimeoutException, Int] = timeout(1.second)(computation3).catching[TimeoutException]
+    println(s"timeout: $res")
+
+  def repeating(): Unit =
+    def computationR: Int =
+      val i = Random.nextInt()
+      println(s"repeating ($i)")
+      i
+
+    repeat(Schedule.fixedInterval(100.millis).maxAttempts(5))(computationR)
--- direct/src/main/scala/de/wittig/ox/StructuredConcurrency2.scala
+++ direct/src/main/scala/de/wittig/ox/StructuredConcurrency2.scala
@@ -7,15 +7,17 @@ import ox.*
 @main
 def structuredConcurrency2(): Unit =
 
+  // supervised Block ist fertig, wenn forKUser fertig ist. fork läuft so lange wie der supervised block aktiv ist und beendet sich dann auch automatisch.
   val a = supervised {
+    fork {
+      while (true) {
+        println("catching Metrics ...")
+        sleep(500.millis)
+      }
+    }
     forkUser {
       println("fork1 start")
       sleep(2.second)
       println("fork1 end")
-    }
-    fork {
-      println("fork2 start")
-      sleep(2.seconds)
-      println("fork2 end")
     }
   }
