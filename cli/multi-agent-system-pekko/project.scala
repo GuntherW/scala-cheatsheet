898a6999af41ba9d5eda18d09426f49d1d1b752a
Gunther Wittig <gunther.wittig@codecentric.de>
2026-09-15 10:32:25 +0200

    WIP

--- cli/multi-agent-system-pekko/project.scala
+++
@@ -1,6 +0,0 @@
-//> using scala 3.3.6
-//> using dep org.apache.pekko::pekko-actor-typed:2.0.0-M4
-//> using dep com.softwaremill.sttp.client4::core:4.0.26
-//> using dep com.lihaoyi::os-lib::0.11.8
-//> using dep com.lihaoyi::upickle::4.4.3
-//> using resourceDir resources
